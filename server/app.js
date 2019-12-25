6cb4cd23cd86d85e34b7850e20634ada38819609
王震英 <wangzyi@aicaigroup.com>
2019-12-25 15:08:20 +0800

    xkugai

--- server/app.js
+++ server/app.js
@@ -7,8 +7,8 @@ const bodyParser = require('body-parser');
 const routerIndex = require('./router/index');
 
 // 抓数据
-const crawler = require('./crawler.js');
-crawler()
+// const crawler = require('./crawler.js');
+// crawler()
 
 app.use(bodyParser.json()); // 处理以json格式的提交
 app.use(bodyParser.urlencoded({ extended: false })); // 处理以form表单的提交
