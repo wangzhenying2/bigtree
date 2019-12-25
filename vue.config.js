8958f3ee8a15b4c6330eb65aa3bb75e4a03e429c
王震英 <wangzyi@aicaigroup.com>
2019-12-25 15:10:32 +0800

    xiugai

--- vue.config.js
+++ vue.config.js
@@ -12,10 +12,10 @@ module.exports = {
         devServer: {
             proxy: {
                 '/msg': {
-                    target: 'http://localhost:3000'
+                    target: 'http://127.0.0.1:3000'
                 },
                 '/prods': {
-                    target: 'http://localhost:3000'
+                    target: 'http://127.0.0.1:3000'
                 }
             }
         }
